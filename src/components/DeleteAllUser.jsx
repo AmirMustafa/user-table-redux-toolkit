61c2b597c0065f50092064e8a6f7842556600b16
Amir Mustafa <amirengg15@gmail.com>
2026-04-08 12:42:13 +0530

    feat: delete all users - redux toolkit

--- src/components/DeleteAllUser.jsx
+++ src/components/DeleteAllUser.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteUsers } from '../store/slices/UserSlice';
+import { MdDeleteForever } from 'react-icons/md';
 
 const DeleteAllUser = () => {
+    const dispatch = useDispatch();
+    const deleteAllUsers = () => {
+        dispatch(deleteUsers())
+    }
   return (
     <div>
-      Delete All Users
-    </div>
+            <button 
+                style={{
+                    backgroundColor: 'red',
+                    color: 'white',
+                    border: 'none',
+                    padding: '6px 12px',
+                    borderRadius: '4px',
+                    cursor: 'pointer',
+                    fontSize: '14px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '5px'
+                }}
+                onClick={() => deleteAllUsers()}
+            >
+                <MdDeleteForever /> Clear Users
+            </button>
+        </div>
   )
 }
 
